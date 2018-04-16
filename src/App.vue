0f59ec7b1d6bc4fdab3f5bc6100ea16b402ce191
Kelvin Au <kelvinau4413@hotmail.com>
2018-04-15 23:35:23 -0700

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="">
+    Home
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+     
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+</style>
