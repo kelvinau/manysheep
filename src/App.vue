c52f68f79819ec858e83235abd3358e94ebdd6b6
Kelvin Au <kelvinau4413@hotmail.com>
2018-04-18 23:57:07 -0700

    Add Floors and license file

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <template>
-  <div class="">
-    Home
+  <div>
+    <floor 
+      v-for="(f, index) in floors" 
+      :left="f[0]" 
+      :top="f[1]"
+      :key="index"></floor>
   </div>
 </template>
 
 <script>
+
+import Floor from '@/components/floor'
+
 export default {
   name: "App",
+  components: {
+    Floor,
+  },
+  mounted() {
+    let sheep = new eSheep(); 
+    sheep.Start(); 
+  },
   data() {
     return {
-     
+      floors: [
+        ['5%', '10%'],
+        ['70%', '30%'],
+        ['28%', '35%'],
+        ['55%', '55%'],
+        ['20%', '75%'],
+      ],
     };
   }
 };
---
+++ src/components/floor.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="floor" :style="positionStyle"></div>
+</template>
+
+<script>
+export default {
+  name: "Floor",
+  props: ['left', 'top'],
+  data() {
+    return {
+     
+    };
+  },
+  computed: {
+      positionStyle() {
+        return {
+            left: this.left,
+            top: this.top,
+        }
+      }
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+    .floor {
+        position: absolute;
+        background-image: url(../assets/floor.jpg);
+        background-size: 100% 100%;
+        width: 30%;
+        height: 50px;
+        border: 1px solid #9E9E9E; // essential for the sheep
+    }
+</style>
